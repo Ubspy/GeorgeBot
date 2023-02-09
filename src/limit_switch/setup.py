d313f70c3ec3fd0ec539d86f5a6274e1a7b734e8
Ubspy <jack-m@live.com>
2023-02-09 12:44:54 -0600

    Updated dependencies

--- src/limit_switch/setup.py
+++ src/limit_switch/setup.py
@@ -11,7 +11,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools', 'lgpio'],
+    install_requires=['setuptools', 'RPi.GPIO'],
     zip_safe=True,
     maintainer='Jack',
     maintainer_email='jack-m@live.com',
